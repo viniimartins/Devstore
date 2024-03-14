8607d5764809df3138db01ce4be1e2f90435efb4
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2024-03-14 00:54:14 -0300

    fix: config yml cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  chromeWebSecurity: false,
+  fixturesFolder: false,
+
   e2e: {
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+
+    supportFile: false,
   },
 })
